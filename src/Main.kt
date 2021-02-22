34a1d6cd9fedab0e73591e67c3db67eed22feb1a
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-02-22 15:05:43 +0300

    Simple Kotlin Concepts

---
+++ src/Main.kt
@@ -0,0 +1,32 @@
+val age = 23;
+val name = "Kamau Mbugua";
+fun main() {
+   printBoder()
+    println("Happy Birthday, ${name}!")
+    printBoder()
+    println("\n")
+    println("\n")
+    
+    
+    println("Happy Birthday ${name}!");
+    print("You are already ${age} \n");
+    println("${age} is the very best age to celebrate, ${name}!");
+    println("\n")
+    
+    println("   ,,,,,   ")
+    println("   |||||   ")
+    println(" =========")
+    println("@@@@@@@@@@@")
+    println("{~@~@~@~@~}")
+    println("@@@@@@@@@@@")
+    println("")
+    
+}
+
+fun printBoder(){
+     repeat(23){
+         print("= ");
+     }
+     println();
+    
+}

da0c2e101d155a8d460ae85f2d2e0bc2302bfeb1
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-02-24 01:02:35 +0300

    Implemented Inheritance

---
+++ src/kotlinInheritance.kt
@@ -0,0 +1,27 @@
+fun main(){
+	//val dweling = Dwelling()
+	
+	val squareCabin = SquareCabin(6)
+
+    println("\nSquare Cabin\n============")
+    println("Capacity: ${squareCabin.capacity}")
+    println("Material: ${squareCabin.buildingMaterial}")
+    println("Has room? ${squareCabin.hasRoom()}")
+	
+}
+
+abstract class Dwelling(private var residents: Int){
+	abstract val buildingMaterial: String
+	abstract val capacity: Int
+	
+	
+	
+	fun hasRoom(): Boolean{
+		return residents < capacity
+	}
+}
+
+class SquareCabin(residents: Int) : Dwelling(residents){
+	override val buildingMaterial = "Wood"
+	override val capacity = 6
+}

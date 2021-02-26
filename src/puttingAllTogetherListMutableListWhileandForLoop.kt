040b2f6f5028e792029564cd73d2c2c53007f8c1
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-02-27 02:32:10 +0300

    Implemented the food order code

--- src/puttingAllTogetherListMutableListWhileandForLoop.kt
+++ src/puttingAllTogetherListMutableListWhileandForLoop.kt
@@ -1,3 +1,28 @@
+class Order(val orderNumber:Int){
+	private val itemList = mutableListOf<Item>()
+	
+	fun addItem(newItem:Item){
+		
+		itemList.add(newItem)
+		
+	}
+	
+	fun addAll(newItems:List<Item>){
+		itemList.addAll(newItems)
+		
+	}
+	
+	fun print(){
+		println("Order #${orderNumber}")
+		var total = 0
+		for (item in itemList){
+			println("${item}:KES ${item.price}")
+		total += item.price
+		}
+		println("Total:KES ${total}")
+		
+	}
+}
 open class Item(val name:String, val price:Int)
 class Noodles: Item("Noodles", 10){
 	override fun toString():String{
@@ -23,4 +48,24 @@ fun main() {
 	println(vegetables)
 	println(vegetables1)
 	
+	println()
+	
+	
+	val order1 = Order(1)
+    order1.addItem(Noodles())
+    order1.print()
+
+    println()
+
+    val order2 = Order(2)
+    order2.addItem(Noodles())
+    order2.addItem(Vagetables())
+    order2.print()
+
+    println()
+
+    val order3 = Order(3)
+    val items = listOf(Noodles(), Vagetables("Carrots", "Beans", "Celery"))
+    order3.addAll(items)
+    order3.print()
 }

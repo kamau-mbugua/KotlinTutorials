9df033ee27753ef5e53a9c4a9e459e8352e37440
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-02-27 02:02:57 +0300

    Made class accept 3 string paramaters

--- src/puttingAllTogetherListMutableListWhileandForLoop.kt
+++ src/puttingAllTogetherListMutableListWhileandForLoop.kt
@@ -4,14 +4,17 @@ class Noodles: Item("Noodles", 10){
 		return name
 	}
 }
-class Vagetables :Item("Vagetables", 15){
+class Vagetables(val topping1:String,
+				 val topping2:String,
+				 val topping3:String,
+) :Item("Vagetables", 15){
 	override fun toString():String{
 		return name
 	}
 }
 fun main() {
 	val noodies = Noodles()
-	val vegetables = Vagetables()
+	val vegetables = Vagetables("Cabbages","Sprouts","Onion")
 	println(noodies)
 	println(vegetables)
 	

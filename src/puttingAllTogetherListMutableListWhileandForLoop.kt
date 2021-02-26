9511694eb3a8315d7ccf5eb5c3b66eaf13d4a0fa
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-02-27 01:58:31 +0300

    Override toString() method

--- src/puttingAllTogetherListMutableListWhileandForLoop.kt
+++ src/puttingAllTogetherListMutableListWhileandForLoop.kt
@@ -1,6 +1,14 @@
 open class Item(val name:String, val price:Int)
-class Noodles: Item("Noodles", 10)
-class Vagetables :Item("Vagetables", 15)
+class Noodles: Item("Noodles", 10){
+	override fun toString():String{
+		return name
+	}
+}
+class Vagetables :Item("Vagetables", 15){
+	override fun toString():String{
+		return name
+	}
+}
 fun main() {
 	val noodies = Noodles()
 	val vegetables = Vagetables()

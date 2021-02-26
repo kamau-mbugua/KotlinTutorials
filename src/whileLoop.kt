26d483741545c487c781beb79e6965f165bd1d5e
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-02-27 01:28:14 +0300

    implemented a simple for loop in kotlin

--- src/whileLoop.kt
+++ src/whileLoop.kt
@@ -2,11 +2,18 @@ fun main(){
 	val guestPerFamily = listOf(2,4,1,3)
 	var totalGuests = 0
 	var index =0
-	
+	//While Loop
 	while(index < guestPerFamily.size){
 		totalGuests += guestPerFamily[index]
 		index++
 	}
 	 println("Total Guest Count: $totalGuests")
-	println("Size of the list is: $guestPerFamily[index]")
+	
+	//For Loop
+	
+	val names = listOf("Jessica", "Henry", "Alicia", "Jose")
+	for(name in names){
+		println(name)
+	}
+
 }

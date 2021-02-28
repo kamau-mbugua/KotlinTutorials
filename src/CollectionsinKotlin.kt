893353eb206917a28bc363544f383d9335cddaef
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-02-28 14:31:41 +0300

    Sorted the list

--- src/CollectionsinKotlin.kt
+++ src/CollectionsinKotlin.kt
@@ -1,6 +1,8 @@
 fun main(){
 	//A collection is a group of related items
 	val numbers = listOf(0,3,8,4,0,5,5,8,9,2)
-	println("List: ${numbers.size}")
+	println("List: ${numbers}")
+	println("List size: ${numbers.size}")
+	println("Sorted List: ${numbers.sorted()}")
 	
 }
